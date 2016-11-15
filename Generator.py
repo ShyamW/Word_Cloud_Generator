9b917e3338232f23ad0d986c048f36295146357d
Shyam Thiagarajan <ShyamW@users.noreply.github.com>
2016-11-15 15:38:17 -0500

    Update Generator.py

--- Generator.py
+++ Generator.py
@@ -1,35 +1,48 @@
-def remove_common_words(line):
-	bad_words =[]
-	"""
-		Although less readable, faster
-	"""
-	with open('badwords.txt') as f:
-		for badword in f:
-			bad_words.append(badword.strip('\n'))
-	for bad in bad_words:
-		line = line.replace(bad);
-	return line;
+class word_cloud:
+	word_file = ''
 	
+	def _init_(self, file_name):
+		self.word_file = file_name
+		
+		
+	def remove_common_words(self,line):
+		bad_words =[]
+		"""
+			Although less readable, faster
+		"""
+		with open('badwords.txt') as f:
+			for badword in f:
+				bad_words.append(badword.strip('\n'))
+		for bad in bad_words:
+			line = line.replace(bad);
+		return line;
+		
 
-def prep_line(line):
-	# remove punctuation
-	punctuation= ",./!?\t\n\t\r()[]|\"
-	for symbol in punction:
-		line.strip(symbol)
-	line = line.replace('\n', ' ').lower()
-	line = remove_common_words(line);
-	return line
+	def prep_line(line):
+		# remove punctuation
+		punctuation= ",./!?\t\n\t\r()[]|\"
+		for symbol in punction:
+			line.strip(symbol)
+		line = line.replace('\n', ' ').lower()
+		line = remove_common_words(line);
+		return line
 
-def get_text():
-	text = ''
-	with open('file.txt') as f:
-		for line in f:
-			line = prep_line(line)
-			text.join(line)
-	return text
-	
+	def get_text():
+		text = ''
+		with open('file.txt') as f:
+			for line in f:
+				line = prep_line(line)
+				text.join(line)
+		return text
+		
+	def generate():
+		text = get_text()
+		//call API
+		
+				
 def main():
-	text = get_text()
+	cloud = word_cloud("file.txt")
+	cloud.generate
 	
-			
+
 main()
