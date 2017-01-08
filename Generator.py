1edc3e3eb833c0ddc310419c409a9dddfe489a57
shyamw <you@example.com>
2017-01-08 15:06:41 -0500

    Started Code Documentation

--- Generator.py
+++ Generator.py
@@ -1,48 +1,57 @@
-class word_cloud:
+class Cloud:
 	word_file = ''
-	
+	common_words_file = 'badwords.txt'
+
+	"""Constructor that assigns word_file location
+	@param file_name
+		path to file with words
+	"""
 	def _init_(self, file_name):
 		self.word_file = file_name
-		
-		
-	def remove_common_words(self,line):
+
+	"""Removes common words from {@code line} (such as a, and, the) stored in badwords.txt
+	@param line
+		line of text to remove common words from
+	"""
+	@staticmethod
+	def remove_common_words(line):
 		bad_words =[]
 		"""
 			Although less readable, faster
 		"""
-		with open('badwords.txt') as f:
+		with open(Cloud.common_words_file) as f:
 			for badword in f:
 				bad_words.append(badword.strip('\n'))
 		for bad in bad_words:
-			line = line.replace(bad);
-		return line;
-		
-
-	def prep_line(line):
-		# remove punctuation
-		punctuation= ",./!?\t\n\t\r()[]|\"
-		for symbol in punction:
-			line.strip(symbol)
-		line = line.replace('\n', ' ').lower()
-		line = remove_common_words(line);
+			line = line.replace(bad)
 		return line
 
+
+	@staticmethod
+	def prep_line(line):
+		# remove punctuation
+		punctuation= ",./!?\t\n\t\r()[]|"
+		for symbol in punctuation:
+			line.strip(symbol)
+		line = line.replace('\n', ' ').lower()
+		line = Cloud.remove_common_words(line)
+		return line
+
+	@staticmethod
 	def get_text():
 		text = ''
 		with open('file.txt') as f:
 			for line in f:
-				line = prep_line(line)
+				line = Cloud.prep_line(line)
 				text.join(line)
 		return text
 		
-	def generate():
-		text = get_text()
+	def generate(self):
+		text = Cloud.get_text()
 		#call APi MethoD
 		
-				
-def main():
-	cloud = word_cloud("file.txt")
-	cloud.generate
-	
 
-main()
+	
+"""API DEMONSTRATION"""
+cloud = Cloud(file_name = "file.txt")
+cloud.generate()
